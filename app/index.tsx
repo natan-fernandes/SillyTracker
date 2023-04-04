3c100a45340cbab6b39817f377ea937b3eed7536
Natan Fernandes <natanfernandes@protonmail.com>
2023-04-04 01:38:02 -0300

    add todo list

--- app/index.tsx
+++ app/index.tsx
@@ -3,6 +3,9 @@ import { StatusBar } from 'expo-status-bar';
 import tw, { useDeviceContext } from 'twrnc';
 import { Map } from './pages/Map';
 
+//TODO: Create button component with <ActivityIndicator/> to wait loading
+//TODO: Add tracking in real time
+//TODO: Add filter markers by date
 
 export default function Page() {
   useDeviceContext(tw);
